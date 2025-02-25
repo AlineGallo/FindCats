941cfdae43da9b84349406679f358ca9899070f7
Aline1002 <alinegallo02@gmail.com>
2025-02-25 16:20:36 -0300

    style: ajustando o btn da tela inicial

--- src/shared/components/CardMeow.vue
+++ src/shared/components/CardMeow.vue
@@ -11,7 +11,7 @@
       <p class="p-2 text-center">Projeto desenvolvido para você conferir os gatinhos a partir de The Cat API</p>
       <p class="p-2 text-center">Siga para a próxima página para encontrar o gatinho certo para você.</p>
       <button @click="$router.push({ name: 'listagem' })"
-        class="h-10 bg-a_secondary w-1/2 border-2 rounded-md text-white m-5">
+        class="h-10 bg-a_secondary w-1/2 border-2 rounded-md text-white m-5 shadow-lg hover:bg-a_primary">
         Buscar
       </button>
     </div>
