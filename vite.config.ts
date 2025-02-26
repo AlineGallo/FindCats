40d5d30e42d0f0fb624f080a57c9f0df012426dc
Aline1002 <alinegallo02@gmail.com>
2025-02-26 10:18:53 -0300

    fix: removendo base

--- vite.config.ts
+++ vite.config.ts
@@ -8,14 +8,14 @@ export default defineConfig({
   plugins: [
     vue(),
   ],
-  base: '/meow/',
+  // base: '/exemplo/',
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url)),
-      '@components': fileURLToPath(new URL('./src/shared/components',  import.meta.url)),
-      '@helpers': fileURLToPath(new URL('./src/shared/helpers',  import.meta.url)),
-      '@services': fileURLToPath(new URL('./src/shared/services',  import.meta.url)),
-      '@stores': fileURLToPath(new URL('./src/shared/stores',  import.meta.url))
+      '@components': fileURLToPath(new URL('./src/shared/components', import.meta.url)),
+      '@helpers': fileURLToPath(new URL('./src/shared/helpers', import.meta.url)),
+      '@services': fileURLToPath(new URL('./src/shared/services', import.meta.url)),
+      '@stores': fileURLToPath(new URL('./src/shared/stores', import.meta.url))
     },
   },
 })
